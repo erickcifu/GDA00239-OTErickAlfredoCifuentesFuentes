27548bd1bc3a185ecb0d126c8b14676f9655be14
erickcifu <erickalfred29@gmail.com>
2025-01-08 03:31:51 -0600

    .

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -15,12 +15,6 @@ const autenticar = (req, res, next) => {
   try {
     const decoded = verificarToken(token, process.env.JWT_SECRET);
     req.user = decoded; 
-    const idCliente = decoded.idCliente;
-
-    if (!idCliente) {
-      return res.status(400).json({ message: "El usuario no tiene un cliente asociado." });
-    }
-
     console.log("Datos del usuario autenticado:", req.user);
     next();
   } catch (error) {
--- src/mitiendaonline/src/components/navbarCliente.js
+++ src/mitiendaonline/src/components/navbarCliente.js
@@ -2,15 +2,11 @@ import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Button, Drawer, List, ListItem, ListItemText, Divider, Alert } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu'; 
 import { Link, useNavigate } from 'react-router-dom';
-import api, {obtenerCliente} from '../config/axios'; 
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
     const [openToken, setOpenToken] = useState(false);
-    const idCliente = localStorage.getItem('Clientes_idClientes');; 
-    const navigate = useNavigate(); 
-
-    console.log("El idCliente es:", idCliente);
+  const navigate = useNavigate();
     const cerrarSesion = () => {
         setOpenToken(true);
         localStorage.removeItem('token'); 
@@ -43,7 +39,7 @@ const Navbar = () => {
               <div sx={{ display: { xs: 'none', md: 'flex' } }}>
                 <Button color="inherit" component={Link} to="/home">Home</Button>
                 <Button color="inherit" component={Link} to="/pedidos">Pedidos</Button>
-                <Button color="inherit" component={Link} to={`/actualizarDatos/${idCliente}`}>Actualizar Datos</Button>
+                <Button color="inherit" component={Link} to="/actualizarDatos">Actualizar Datos</Button>
                 <Button color="inherit" onClick={cerrarSesion} component={Link} to="/">Cerrar Sesión</Button>
               </div>
             </Toolbar>
@@ -61,7 +57,7 @@ const Navbar = () => {
                 <ListItem button component={Link} to="/pedidos">
                   <ListItemText primary="Pedidos" />
                 </ListItem>
-                <ListItem button component={Link} to={`/actualizarDatos/${idCliente || ''}`}>
+                <ListItem button component={Link} to="/actualizarDatos">
                   <ListItemText primary="Actualizar Datos" />
                 </ListItem>
                 <ListItem onClick={cerrarSesion} button component={Link} to="/">
